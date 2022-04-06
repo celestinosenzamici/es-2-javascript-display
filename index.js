e92456a9d82d2df503f50c4274e2b622a0545f2b
=celestino <=celestinosenzamici@hotmail.it>
2022-04-06 10:59:24 +0200

    celestino

---
+++ index.js
@@ -0,0 +1,8 @@
+var bottone=document.getElementById("mostra");
+    
+bottone.addEventListener("click",mostra);
+function mostra(){
+        var div =document.getElementById("messaggio");
+        div.style.display="block";
+}
+
